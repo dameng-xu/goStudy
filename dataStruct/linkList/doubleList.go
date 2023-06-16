3dc6353beb8188a13f1254eb6f6d101135ffeb61
xudameng <xudameng@moresec.cn>
2023-06-16 13:46:11 +0800

    添加双向链表增删改差操作

--- dataStruct/linkList/doubleList.go
+++ dataStruct/linkList/doubleList.go
@@ -1,71 +1,162 @@
 package linkList
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type DoubleNode struct {
-	Data interface{}
-	Pre  *DoubleNode
-	Next *DoubleNode
+// DNode represents a DNode in the doubly linked list
+type DNode struct {
+	data interface{}
+	prev *DNode
+	next *DNode
 }
 
-type DoubleLinkList struct {
-	Head *DoubleNode
-	Tail *DoubleNode
+// DoublyLinkedList represents the doubly linked list
+type DoublyLinkedList struct {
+	head *DNode
+	tail *DNode
 }
 
-func (d *DoubleLinkList) Append(value interface{}) {
-	newNode := DoubleNode{
-		Data: value,
-		Pre:  nil,
-		Next: nil,
-	}
-	if d.Head == nil {
-		d.Head = &newNode
-		d.Tail = &newNode
-		return
-	}
-	d.Tail.Next = &newNode
-	newNode.Pre = d.Tail
-	d.Tail = &newNode
-	return
-}
+// InsertAtEnd inserts a new DNode with the given data at the end of the list
+func (dll *DoublyLinkedList) InsertAtEnd(data interface{}) {
+	newDNode := &DNode{data: data, prev: nil, next: nil}
 
-func (d *DoubleLinkList) Add(value interface{}) {
-	newNode := DoubleNode{
-		Data: value,
-		Pre:  nil,
-		Next: nil,
-	}
-	if d.Head == nil {
-		d.Head = &newNode
-		d.Tail = &newNode
+	if dll.head == nil {
+		dll.head = newDNode
+		dll.tail = newDNode
 		return
 	}
 
-	newNode.Next = d.Head
-	d.Head.Pre = &newNode
-	d.Head = &newNode
+	current := dll.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	newDNode.prev = current
+	current.next = newDNode
+	dll.tail = newDNode
+}
+
+// Add insert a new DNode in the head place
+func (dll *DoublyLinkedList) Add(data interface{}) {
+	if dll.head == nil {
+		dll.InsertAtEnd(data)
+		return
+	}
+	dll.head = &DNode{
+		prev: nil,
+		data: data,
+		next: dll.head,
+	}
 	return
 }
 
-func (d *DoubleLinkList) PrintForward() {
-	currentNode := d.Head
-
-	fmt.Print("DoublyLinkedList (Forward): ")
-	for currentNode != nil {
-		fmt.Printf("%d -> ", currentNode.Data)
-		currentNode = currentNode.Next
+func (dll *DoublyLinkedList) Insert(index int, data interface{}) {
+	if dll.head == nil {
+		return
 	}
+	if index == 0 {
+		dll.Add(data)
+		return
+	}
+	pre := dll.head
+	curIdx := 1
+	cur := pre.next
+
+	for cur != nil {
+		if curIdx == index {
+			newNode := DNode{
+				data: data,
+				prev: pre,
+				next: cur,
+			}
+			pre.next = &newNode
+			cur.prev = &newNode
+			return
+		}
+		pre = cur
+		cur = cur.next
+		curIdx++
+	}
+}
+
+// Delete deletes the first occurrence of the DNode with the given data from the list
+func (dll *DoublyLinkedList) Delete(index int) {
+	pre := dll.head
+	if pre == nil {
+		return
+	}
+	if index == 0 {
+		next := dll.head.next
+		dll.head = next
+		if next == nil {
+			dll.tail = nil
+			return
+		}
+		next.prev = nil
+		return
+	}
+	curIndex := 1
+	current := pre.next
+	for current != nil {
+		if curIndex == index {
+			next := current.next
+			pre.next = next
+			if next != nil {
+				next.prev = pre
+				return
+			}
+			dll.tail = pre
+			return
+		}
+		pre = current
+		current = current.next
+		curIndex++
+	}
+}
+
+// Get there value in the index index
+func (dll *DoublyLinkedList) Get(index int) interface{} {
+	curNode := dll.head
+	curIndex := 0
+	for curNode != nil {
+		if curIndex == index {
+			return curNode.data
+		}
+		curNode = curNode.next
+		curIndex++
+	}
+	return nil
+}
+
+// Reverse reverses the order of the DNodes in the list
+func (dll *DoublyLinkedList) Reverse() {
+	current := dll.head
+	var prev *DNode
+
+	for current != nil {
+		next := current.next
+		current.next = prev
+		current.prev = next
+		prev = current
+		current = next
+	}
+
+	dll.head = prev
+}
+
+// Display prints the elements of the list
+func (dll *DoublyLinkedList) Display() {
+	current := dll.head
+
+	for current != nil {
+		fmt.Print(current.data, " ")
+		current = current.next
+	}
+
 	fmt.Println()
 }
 
-func (d *DoubleLinkList) PrintBackward() {
-	currentNode := d.Tail
-
-	fmt.Print("DoublyLinkedList (Backward): ")
-	for currentNode != nil {
-		fmt.Printf("%d <- ", currentNode.Data)
-		currentNode = currentNode.Pre
-	}
-	fmt.Println()
+func (dll *DoublyLinkedList) PrintTrail() {
+	fmt.Println("tail: ", dll.tail)
 }
--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dameng-xu/goStudy/dataStruct/linkList"
 )
 
 func main() {
-	l := linkList.LinkList{}
-	l.Add(1)
-	l.Add(2)
-	l.Add(3)
-	l.Add(4)
-	l.Add(5)
-	l.Add(6)
-	l.Append(0)
-	l.Append(100000000)
-	l.Insert(0, 100)
-	l.Insert(3, 101)
-	l.Insert(9, 999)
-	l.PrintList()
-	l.Del(10)
-	l.PrintList()
+	dll := linkList.DoublyLinkedList{}
+
+	dll.InsertAtEnd(1)
+	dll.InsertAtEnd(2)
+	dll.InsertAtEnd(3)
+	dll.InsertAtEnd(4)
+	dll.InsertAtEnd(5)
+
+	fmt.Println("Original list:")
+	dll.Display()
+	dll.Insert(4, 100)
+	dll.PrintTrail()
+	dll.Display()
 }
